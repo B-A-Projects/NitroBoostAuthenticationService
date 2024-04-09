e4836036b507748e939186f8badea777ab50b0ea
Bluerrito <yannick.d.b114@gmail.com>
2024-04-09 16:49:30 +0200

    Added test project

---
+++ NitroBoostAuthenticationService.Test/AuthenticationTests.cs
@@ -0,0 +1,10 @@
+namespace NitroBoostAuthenticationService.Test;
+
+public class AuthenticationTests
+{
+    [Fact]
+    public void Test_Dummy_AlwaysTrue()
+    {
+        Assert.True(true);
+    }
+}
